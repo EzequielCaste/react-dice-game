9286f3b7ac07fc86aafe706ff693a5678119d267
EzequielCaste <ezequiel.caste@gmail.com>
2020-10-26 13:42:27 -0300

    Updates

---
+++ precache-manifest.52d5d58fe48cecf78599bb5ac9fa7487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a64ddd1f4e370359f4c8bac4f23aed",
+    "url": "/react-dice-game/index.html"
+  },
+  {
+    "revision": "70531a10b6ce0610f5e4",
+    "url": "/react-dice-game/static/css/main.2c5f39cb.chunk.css"
+  },
+  {
+    "revision": "e56aba1245456da9ba26",
+    "url": "/react-dice-game/static/js/2.148642ed.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/react-dice-game/static/js/2.148642ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70531a10b6ce0610f5e4",
+    "url": "/react-dice-game/static/js/main.cb898e16.chunk.js"
+  },
+  {
+    "revision": "526f5bc0a4d8d589468d",
+    "url": "/react-dice-game/static/js/runtime-main.f40a78c4.js"
+  }
+]);
